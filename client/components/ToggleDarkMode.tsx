9c298acc27dec297772b522093c4c966140fa58f
mixnuts07 <french0719@gmail.com>
2022-10-08 15:33:24 +0900

    いったん終了。

--- client/components/ToggleDarkMode.tsx
+++ client/components/ToggleDarkMode.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 
-const ToggleDarkMode = (): string => {
+const ToggleDarkMode = (): JSX.Element => {
   const [darkMode, setDarkMode] = useState<string>("dark");
   const toggleMode = () => {
     darkMode === "dark" ? setDarkMode("") : setDarkMode("dark");
   };
-  return darkMode;
+  // return darkMode;
+  return <button onClick={toggleMode}>Toggle Mode</button>;
 };
 export default ToggleDarkMode;
